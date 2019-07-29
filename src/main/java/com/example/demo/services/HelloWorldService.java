bac9f5a660a32c4025af78c0316460b4a925e512
audiomojo <audiomojo@yahoo.com>
2019-07-29 11:14:24 -0600

    Forgot to add @Service annotation

--- src/main/java/com/example/demo/services/HelloWorldService.java
+++ src/main/java/com/example/demo/services/HelloWorldService.java
@@ -1,5 +1,8 @@
 package com.example.demo.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloWorldService {
     public String helloWorld() {
         return "Hello World";
